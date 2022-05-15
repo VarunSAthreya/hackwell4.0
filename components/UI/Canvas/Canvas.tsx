c12008508d95c2a7e5eacc2773c98699617e3587
Varun S Athreya <varun.sathreya@gmail.com>
2022-05-16 00:14:50 +0530

    Changed main animation

--- components/UI/Canvas/Canvas.tsx
+++ components/UI/Canvas/Canvas.tsx
@@ -1,114 +1,20 @@
 import React, { useEffect } from 'react';
-import { ImageCode1 } from '../../../util/ImageCode';
+import { canvas, utils } from './utils';
 
-const Canvas = (): JSX.Element => {
+const Canvas = () => {
     useEffect(() => {
-        const myImage = new Image();
-        myImage.src = ImageCode1;
-        myImage.addEventListener('load', function () {
-            const canvas = document.getElementById('canvas1') as any;
-            const ctx = canvas.getContext('2d');
-            let width = window.innerWidth;
-            if (width > 500) width -= 20;
-            canvas.width = width;
-            canvas.height = window.innerHeight;
+        utils(window);
 
-            ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
-            const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
-            let particlesArray = [];
-            const numberOfParticles = 5000;
-
-            let mappedImage = [];
-            for (let y = 0; y < canvas.height; y++) {
-                let row = [];
-                for (let x = 0; x < canvas.width; x++) {
-                    const red = pixels.data[y * 4 * pixels.width + x * 4];
-                    const green =
-                        pixels.data[y * 4 * pixels.width + (x * 4 + 1)];
-                    const blue =
-                        pixels.data[y * 4 * pixels.width + (x * 4 + 2)];
-                    const brightness = calculateRelativeBrightness(
-                        red,
-                        green,
-                        blue
-                    );
-                    const cell = [brightness];
-                    row.push(cell);
-                }
-                mappedImage.push(row);
-            }
-
-            function calculateRelativeBrightness(red, green, blue) {
-                return (
-                    Math.sqrt(red + red + (green + green) + (blue + blue)) / 55
-                );
-            }
-
-            class Particle {
-                x: number;
-                y: number;
-                speed: number;
-                velocity: number;
-                size: number;
-                position1: number;
-                position2: number;
-                constructor() {
-                    this.x = Math.random() * canvas.width;
-                    this.y = 0;
-                    this.speed = 0;
-                    this.velocity = Math.random() * 3.0;
-                    this.size = Math.random() * 2.5 + 1;
-                    this.position1 = Math.floor(this.y);
-                    this.position2 = Math.floor(this.x);
-                }
-                update() {
-                    this.position1 = Math.floor(this.y);
-                    this.position2 = Math.floor(this.x);
-                    this.speed = mappedImage[this.position1][this.position2][0];
-                    let movement = 2.5 - this.speed + this.velocity;
-                    this.y += movement;
-                    if (this.y >= canvas.height) {
-                        this.y = 0;
-                        this.x = Math.random() * canvas.width;
-                    }
-                }
-                draw() {
-                    ctx.beginPath();
-                    ctx.fillStyle = '#CC01FF';
-                    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-                    ctx.fill();
-                }
-            }
-            function init() {
-                for (let i = 0; i < numberOfParticles; i++) {
-                    particlesArray.push(new Particle());
-                }
-            }
-            init();
-            function animate() {
-                ctx.globalAlpha = 0.05;
-                ctx.fillStyle = 'rgb(0, 0, 0)';
-                ctx.fillRect(0, 0, canvas.width, canvas.height);
-                ctx.globalAlpha = 0.02;
-                for (const element of particlesArray) {
-                    element.update();
-                    ctx.globalAlpha = element.speed * 0.5;
-                    element.draw();
-                }
-                requestAnimationFrame(animate);
-            }
-            animate();
-        });
+        canvas(window, document.querySelectorAll.bind(document));
     }, []);
 
     return (
         <canvas
-            id="canvas1"
+            className="canvas"
+            width="400"
+            height="400"
             style={{
-                zIndex: -10,
-                opacity: 0.6,
-                height: '100vh',
+                backgroundColor: '#000',
             }}
         ></canvas>
     );
