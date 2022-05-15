053fac2659f2c6237267fec668d3221548fcd861
Varun S Athreya <varun.sathreya@gmail.com>
2022-05-15 18:13:38 +0530

    Increased trasition delay

--- util/variants.tsx
+++ util/variants.tsx
@@ -1,79 +1,91 @@
 export const stack = {
     hidden: {
-      opacity: 0,
+        opacity: 0,
     },
     visible: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.4,
-      },
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.4,
+        },
     },
     exit: {
-      opacity: 0,
-      transition: {
-        delay: 0.2,
-        duration: 0.5,
-      },
+        opacity: 0,
+        transition: {
+            delay: 0.2,
+            duration: 0.5,
+        },
     },
-  };
+};
 
-  export const innerStack = {
+export const innerStack = {
     hidden: {
         opacity: 0,
-        x:-80
-      },
-      whileInView: {
-        opacity: 1,
-        x:0
+        x: -80,
     },
-      exit: {
+    whileInView: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            delay: 0.2,
+            duration: 0.8,
+        },
+    },
+    exit: {
         opacity: 0,
-        x:-80,
-      },
-  }
+        x: -80,
+    },
+};
 
-  export const image = {
+export const image = {
     hidden: {
         opacity: 0,
-        x:100
-      },
-      whileInView: {
-        opacity: 1,
-        x:0
+        x: 100,
     },
-      exit: {
+    whileInView: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            delay: 0.2,
+            duration: 0.8,
+        },
+    },
+    exit: {
         opacity: 0,
-        x:100,
-      },
-  }
+        x: 100,
+    },
+};
 
-  export const secondaryImage = {
+export const secondaryImage = {
     hidden: {
         opacity: 0,
-        x:10
-      },
-      whileInView: {
-        opacity: 1,
-        x:0
+        x: 10,
     },
-      exit: {
+    whileInView: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            delay: 0.2,
+            duration: 0.8,
+        },
+    },
+    exit: {
         opacity: 0,
-        x:10,
-      },
-  }
+        x: 10,
+    },
+};
 
-  export const divider = {
-      hidden: { width: 0 },
-      whileInView: {
-          width: '100%',
-          transition: {
-              delay: 0.2,
-              duration: 0.4,
-          },
-      },
-  };
+export const divider = {
+    hidden: { width: 0 },
+    whileInView: {
+        width: '100%',
+        transition: {
+            delay: 0.2,
+            duration: 1,
+        },
+    },
+};
 
-  export const secondaryDivider = {
+export const secondaryDivider = {
     hidden: { width: 0 },
     whileInView: {
         width: '10%',
@@ -82,113 +94,148 @@ export const stack = {
             duration: 0.8,
         },
     },
-  }
+};
 
-  export const aspectRatio = {
+export const footerDivider = {
+    hidden: { width: 0 },
+    whileInView: {
+        width: '90%',
+        transition: {
+            delay: 0.2,
+            duration: 0.8,
+        },
+    },
+};
+
+export const aspectRatio = {
     hidden: {
         opacity: 0,
-        x:-10
-      },
-      whileInView: {
-        opacity: 1,
-        x:0
+        x: -10,
     },
-      exit: {
+    whileInView: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            delay: 0.2,
+            duration: 0.8,
+        },
+    },
+    exit: {
         opacity: 0,
-        x:-10
-      },
-  }
+        x: -10,
+    },
+};
 
-  export const secondaryStack = {
+export const secondaryStack = {
     hidden: {
         opacity: 0,
-        x:100
-      },
-      whileInView: {
-        opacity: 1,
-        x:0
+        x: 100,
     },
-      exit: {
+    whileInView: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            delay: 0.2,
+            duration: 0.8,
+        },
+    },
+    exit: {
         opacity: 0,
-        x:100,
-      },
-  }
+        x: 100,
+    },
+};
 
-  export const box = {
+export const box = {
     hidden: {
         opacity: 0,
-        border:0
-      },
-      whileInView: {
-        opacity: 1,
-        border:'1px solid #2d3748'
+        border: 0,
     },
-      exit: {
+    whileInView: {
+        opacity: 1,
+        border: '1px solid #2d3748',
+        transition: {
+            delay: 0.2,
+            duration: 0.8,
+        },
+    },
+    exit: {
         opacity: 0,
-        border:0
-      },
-  }
+        border: 0,
+    },
+};
 
-  export const gridItem = {
+export const gridItem = {
     hidden: {
         opacity: 0,
-        y:-80
-      },
-      whileInView: {
-        opacity: 1,
-        y:0
+        y: -80,
     },
-      exit: {
+    whileInView: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            delay: 0.2,
+            duration: 0.8,
+        },
+    },
+    exit: {
         opacity: 0,
-        y:-80,
-      },
-  }
+        y: -80,
+    },
+};
 
-  export const secondaryGridItem = {
+export const secondaryGridItem = {
     hidden: {
         opacity: 0,
-        y:-80
-      },
-      whileInView: {
-        opacity: 1,
-        y:0
+        y: -80,
     },
-      exit: {
+    whileInView: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            delay: 0.2,
+            duration: 0.8,
+        },
+    },
+    exit: {
         opacity: 0,
-        y:-80,
-      },
-  }
+        y: -80,
+    },
+};
 
-  export const simpleGrid = {
-      hidden: {
-          opacity: 0,
-      },
-      visible: {
-          opacity: 1,
-          transition: {
-              staggerChildren: 0.2,
-          },
-      },
-      exit: {
-          opacity: 0,
-          transition: {
-              delay: 0.2,
-              duration: 0.5,
-          },
-      },
-  };
-
-  export const ternaryStack = {
+export const simpleGrid = {
     hidden: {
         opacity: 0,
-        y:80
-      },
-      whileInView: {
-        opacity: 1,
-        y:0
     },
-      exit: {
+    visible: {
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.2,
+        },
+    },
+    exit: {
         opacity: 0,
-        y:80,
-      },
-  }
+        transition: {
+            delay: 0.2,
+            duration: 0.5,
+        },
+    },
+};
+
+export const ternaryStack = {
+    hidden: {
+        opacity: 0,
+        y: 80,
+    },
+    whileInView: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            delay: 0.2,
+            duration: 0.8,
+        },
+    },
+    exit: {
+        opacity: 0,
+        y: 80,
+    },
+};
